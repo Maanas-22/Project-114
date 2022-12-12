a3038d3112c0eb32fe296f25dc9394d943c02d39
Maanas-22 <89596906+Maanas-22@users.noreply.github.com>
2022-12-12 02:17:45 -0800

    Add files via upload

---
+++ Project 114.py
@@ -0,0 +1,28 @@
+#Code 1
+a = 33
+b = 33
+if b > a :
+    print("b is greater than a")
+elif a == b :
+    print("a and b are equal")
+
+'''
+Output
+
+a and b are equal
+'''
+
+#Code 2
+def my_function(fname):
+    print(fname, "Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+'''
+Output
+
+Emil Refsnes
+Tobias Refsnes
+Linus Refsnes
+'''
